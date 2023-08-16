95390765d7bcb4f6fa862b338186e3660aeffc32
Ta-ti-Alex <122518141+Ta-ti-Alex@users.noreply.github.com>
2023-08-16 11:40:59 +0300

    Add files via upload

---
+++ pythonProject9/avitoparser/spiders/avito.py
@@ -0,0 +1,17 @@
+import scrapy
+from scrapy.http import HtmlResponse
+
+
+class AvitoSpider(scrapy.Spider):
+    name = "avito"
+    allowed_domains = ["avito.ru"]
+    #allowed_domains = ["youla.ru"]
+    #allowed_domains = ["hh.ru"]
+    #start_urls = ["https://www.hh.ru/"]
+    #https://www.avito.ru/nizhniy_novgorod/avtomobili?radius=200&searchRadius=200
+    #start_urls = ["https://www.youla.ru/"]
+    start_urls = ["https://www.avito.ru/nizhniy_novgorod/"]
+
+    def parse(self, response:HtmlResponse):
+        #pass
+        print(response.url)
